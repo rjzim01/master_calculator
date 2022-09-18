8daab55b11b5fc485f0651221a7346c28a1188ed
rjzim01 <rjzim01@gmail.com>
2022-09-18 10:53:54 +0600

    2nd_commit_done

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,24 +1,20 @@
 package com.example.calculator;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-
     TextView tv;
-    Button one,two,three,four,five,six,seven,eight,nine,plus,minus,mul,div,equal,zero,dot,c,operation,mod;
+    Button one,two,three,four,five,six,seven,eight,nine,plus,minus,mul,div,equal,zero,dot,
+            c,operation,mod;
     boolean addition,subtraction,multiplication,division,percent;
     float value1,value2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         TextView tv = findViewById(R.id.tv);
         Button c = findViewById(R.id.btnC);
         Button operation = findViewById(R.id.btnOperation);
@@ -39,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         Button equal = findViewById(R.id.btnEqual);
         Button zero = findViewById(R.id.btn0);
         Button dot = findViewById(R.id.btnDot);
-
         c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -112,6 +107,92 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(tv.getText()+"9");
             }
         });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tv == null)
+                {
+                    tv.setText("");
+                }else{
+                    value1=Float.parseFloat(tv.getText() + "");
+                    addition = true;
+                    tv.setText(null);
+                }
+            }
+        });
 
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tv == null)
+                {
+                    tv.setText("");
+                }else{
+                    value1=Float.parseFloat(tv.getText() + "");
+                    subtraction = true;
+                    tv.setText(null);
+                }
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tv == null)
+                {
+                    tv.setText("");
+                }else{
+                    value1=Float.parseFloat(tv.getText() + "");
+                    multiplication = true;
+                    tv.setText(null);
+                }
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tv == null)
+                {
+                    tv.setText("");
+                }else{
+                    value1=Float.parseFloat(tv.getText() + "");
+                    division = true;
+                    tv.setText(null);
+                }
+            }
+        });
+
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value2 = Float.parseFloat(tv.getText()+"");
+                if(addition==true){
+                    tv.setText(value1+value2+"");
+                    addition=false;
+                }
+                if(subtraction==true){
+                    tv.setText(value1-value2+"");
+                    subtraction=false;
+                }
+                if(multiplication==true){
+                    tv.setText(value1*value2+"");
+                    multiplication=false;
+                }
+                if(division==true){
+                    try{
+                        tv.setText(value1/value2+"");
+                        division=false;
+                    }catch(Exception e){
+                        Log.d("Calc",e.getMessage());
+                    }
+                }}
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("");
+            }
+        });
     }
 }
